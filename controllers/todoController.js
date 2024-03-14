0636b2792d436ad4f47e524dde4a02f063970a30
wiut00015688 <wiut00015688@gmail.com>
2024-03-14 19:42:33 +0500

    created list of objects for tasks and displayed them on main page using ejs

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,7 @@
+var data = [{ item: 'run marathon' }, { item: 'eat pills' }, { item: 'finish CW' }];
 module.exports = function (app) {
    app.get('/todo', function (req, res) {
-      res.render('todo');
+      res.render('todo', { todos: data });
    });
 
    app.post('/todo', function (req, res) {
